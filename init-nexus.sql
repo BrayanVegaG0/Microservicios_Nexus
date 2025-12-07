3b4ca0e8db23f45a8a1904beb1b32ddb6d944bf6
Brayan Vega <bjvega4@espe.edu.ec>
2025-12-07 18:07:32 -0500

    Dockerizada

---
+++ init-nexus.sql
@@ -0,0 +1,5 @@
+-- Bases de datos exclusivas para NEXUS (Banco ID 3)
+CREATE DATABASE db_nexus_cbs;
+CREATE DATABASE db_nexus_transacciones;
+CREATE DATABASE db_nexus_geografia;
+CREATE DATABASE db_nexus_clientes;
